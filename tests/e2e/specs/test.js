0d0e3ebb86ad6f8c0fc4699bb1161474de569694
Валериан Воротников <valerian7202@gmail.com>
2023-06-14 20:03:35 +0300

    end to end first tests

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,26 @@
 // https://docs.cypress.io/api/table-of-contents
 
-describe("My First Test", () => {
-  it("Visits the app root url", () => {
+describe("ballancing interface", () => {
+  it("it has basic display blocks", () => {
     cy.visit("/");
-    cy.contains("h1", "Welcome to Your Vue.js + TypeScript App");
+    cy.contains("div", "Элементы");
+    cy.contains("div", "Итоговый баланс");
+    cy.contains("div", "Информация");
+  });
+
+  it("it has redactor", () => {
+    cy.visit("/");
+    cy.contains("a.btn", "Изменить Модули").click();
+    cy.contains("button", "экспорт");
+    cy.contains("div", "импорт");
+  });
+
+  it("it can change ballance", () => {
+    cy.visit("/");
+    cy.contains("button", " + ").click();
+    cy.contains("button", " + ").click();
+    cy.contains("button", " - ").click();
+
+    cy.contains("li", "healthBoost : 28.57");
   });
 });
